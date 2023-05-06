21c47c3b815a0d145bf2893db9ba0d3739842114
OmerJauhar <umerjouhar31@gmail.com>
2023-05-06 12:29:50 +0500

    removing doc files

--- crates/parser/target/doc/implementors/core/convert/trait.TryFrom.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"sqlparser":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"enum\" href=\"sqlparser/ast/enum.Statement.html\" title=\"enum sqlparser::ast::Statement\">Statement</a>&gt; for <a class=\"struct\" href=\"sqlparser/ast/helpers/stmt_create_table/struct.CreateTableBuilder.html\" title=\"struct sqlparser::ast::helpers::stmt_create_table::CreateTableBuilder\">CreateTableBuilder</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
